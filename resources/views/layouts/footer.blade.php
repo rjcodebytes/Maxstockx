0d228ebe622e4b5ff83d16a8aae69d23f1f91280
rjcodebytes <136474535+rjcodebytes@users.noreply.github.com>
2025-01-13 02:17:49 +0530

    Footer's layout is updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,71 +1,69 @@
 <footer class="footer position-relative" style="z-index: 1; margin-top: 50px;">
     <div class="container">
-        <div class="row">
+        <div class="d-flex justify-content-around flex-wrap align-items-start mt-5">
             <!-- Logo Section -->
-            <div class="col-md-3 mb-4">
+            <div class="col ftr-logo text-center mb-5 me-4">
                 <a class="navbar-brand" href="">
                     <img src="{{ asset('assets/img/logo.png') }}" width="150" height="100" class="ms-5 mt-5">
                 </a>
             </div>
 
             <!-- Maxstockx Section -->
-            <div class="col-md-2 mb-4">
-                <h5>Maxstockx</h5>
+            <div class="col mb-3">
+                <h5 class="h5">Maxstockx</h5>
                 <ul class="list-unstyled">
-                    <li><a href="{{ url('login') }}" class="text-decoration-none text-white">About Us</a></li>
-                    <li><a href="{{ url('blog') }}" class="text-decoration-none text-white">Blog</a></li>
-                    <li><a href="{{ url('contact') }}" class="text-decoration-none text-white">Contact Us</a></li>
+                    <li><a href="{{ url('login') }}" class="text-decoration-none ftr-lnks">About Us</a></li>
+                    <li><a href="{{ url('blog') }}" class="text-decoration-none ftr-lnks">Blog</a></li>
+                    <li><a href="{{ url('contact') }}" class="text-decoration-none ftr-lnks">Contact Us</a></li>
                 </ul>
             </div>
 
             <!-- Quick Links Section -->
-            <div class="col-md-2 mb-4">
-                <h5>Quick Links</h5>
+            <div class="col mb-3">
+                <h5 class="h5">Quick Links</h5>
                 <ul class="list-unstyled">
-                    <li><a href="{{ url('courses') }}" class="text-decoration-none text-dark">Courses</a></li>
-                    <li><a href="{{ url('pricing') }}" class="text-decoration-none text-dark">Pricing</a></li>
-                    <li><a href="{{ url('testimonials') }}" class="text-decoration-none text-dark">Testimonials</a></li>
-                    <li><a href="{{ url('resources') }}" class="text-decoration-none text-dark">Resources</a></li>
+                    <li><a href="{{ url('courses') }}" class="text-decoration-none  ftr-lnks">Courses</a></li>
+                    <li><a href="{{ url('pricing') }}" class="text-decoration-none k ftr-lnks">Pricing</a></li>
+                    <li><a href="{{ url('testimonials') }}" class="text-decoration-none  ftr-lnks">Testimonials</a></li>
+                    <li><a href="{{ url('resources') }}" class="text-decoration-none  ftr-lnks">Resources</a></li>
                 </ul>
             </div>
 
             <!-- Support Section -->
-            <div class="col-md-2 mb-4">
-                <h5>Support</h5>
+            <div class="col mb-3">
+                <h5 class="h5">Support</h5>
                 <ul class="list-unstyled">
-                    <li><a href="{{ url('faqs') }}" class="text-decoration-none text-dark">FAQs</a></li>
-                    <li><a href="{{ url('help-center') }}" class="text-decoration-none text-dark">Help Center</a></li>
-                    <li><a href="{{ url('privacy-policy') }}" class="text-decoration-none text-dark">Privacy Policy</a></li>
-                    <li><a href="{{ url('terms-conditions') }}" class="text-decoration-none text-dark">Terms & Conditions</a></li>
+                    <li><a href="{{ url('faqs') }}" class="text-decoration-none ftr-lnks">FAQs</a></li>
+                    <li><a href="{{ url('help-center') }}" class="text-decoration-none ftr-lnks">Help Center</a></li>
+                    <li><a href="{{ url('privacy-policy') }}" class="text-decoration-none ftr-lnks">Privacy Policy</a></li>
+                    <li><a href="{{ url('terms-conditions') }}" class="text-decoration-none ftr-lnks">Terms & Conditions</a></li>
                 </ul>
             </div>
 
             <!-- Follow Us Section -->
-            <div class="col-md-3 mb-4">
-                <h5>Follow Us on</h5>
-                <div class="d-flex">
-                    <a href="https://facebook.com" target="_blank" class="me-3 text-dark">
+            <div class="col mb-3">
+                <h5 class="h5">Follow Us on</h5>
+                <div class="d-flex social-cont">
+                    <a href="https://facebook.com" target="_blank" class="me-3 ftr-lnks social">
                         <i class="fab fa-facebook fa-lg"></i>
                     </a>
-                    <a href="https://instagram.com" target="_blank" class="me-3 text-dark">
+                    <a href="https://instagram.com" target="_blank" class="me-3 ftr-lnks social">
                         <i class="fab fa-instagram fa-lg"></i>
                     </a>
-                    <a href="https://youtube.com" target="_blank" class="me-3 text-dark">
+                    <a href="https://youtube.com" target="_blank" class="me-3 ftr-lnks social">
                         <i class="fab fa-youtube fa-lg"></i>
                     </a>
-                    <a href="https://twitter.com" target="_blank" class="text-dark">
+                    <a href="https://twitter.com" target="_blank" class="ftr-lnks social">
                         <i class="fab fa-twitter fa-lg"></i>
                     </a>
                 </div>
             </div>
-        </div>
 
-        <!-- Get in Touch Section -->
-        <div class="row mt-4">
-            <div class="col-md-12 text-center">
-                <h5>Get in Touch</h5>
-                <p>Email: <a href="mailto:support@maxstockx.com" class="text-decoration-none text-dark">support@maxstockx.com</a></p>
-                <p>Phone: +1 (123) 456-7890</p>
+            <!-- Get in Touch Section -->
+            <div class="col text-center mb-3">
+                <h5 class="h5">Get in Touch</h5>
+                <p class="ftr-lnks">Email: <a href="mailto:support@maxstockx.com" class="text-decoration-none ftr-lnks">support@maxstockx.com</a></p>
+                <p class="ftr-lnks">Phone: +1 (123) 456-7890</p>
             </div>
         </div>
     </div>
